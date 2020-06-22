65c72597c13aadba35de9e355d144b1650b7ff5e
jkdearnley <jkdearnley@gmail.com>
2020-06-22 23:01:01 +1200

    Contact form initial setup

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,14 +1,8 @@
 import firebase from 'firebase'
 
 var firebaseConfig = {
-  apiKey: "AIzaSyB8jvXH_hihtt1DjdTsY4h3aiRO_QF5N6w",
-  authDomain: "web701-eggcellent.firebaseapp.com",
-  databaseURL: "https://web701-eggcellent.firebaseio.com",
-  projectId: "web701-eggcellent",
-  storageBucket: "web701-eggcellent.appspot.com",
-  messagingSenderId: "101664882857",
-  appId: "1:101664882857:web:3c9c0ed83efe0dbf2ea9b8"
-  };
+// firebase apiKeys
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
