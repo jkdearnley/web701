3860fc6a06891952a1f78578bbd3b3f0552a78b2
jkdearnley <jkdearnley@gmail.com>
2020-06-22 16:21:03 +1200

    About edited

--- src/components/about.vue
+++ src/components/about.vue
@@ -3,29 +3,7 @@
 	<div class="container">
 		<div class="col-12">
 			<h1>A Little About Us!</h1>
-			<p>Blake Stevens and John Steviki started CompuXIT in 2011. The company 
-			began life as a networking service provider operated in John's garage 
-			as a side project to their current IT based jobs. A small database was 
-			setup and the cloud storage services were offered to Blake and Johns' 
-			friends and family </p>
-			<p>While they charged them very little, if at all, they saw the potential 
-			this garage hosted little business had and quickly moved the business 
-			to a small rented office space in Nelson city in 2013. With minimal 
-			advertisement, it didn't take the duo long at all to gain a reputation 
-			around Nelson and the company's customer base continued to grow, with 
-			regulars that now rely purely on CXIT's services. John and Blake left 
-			their full time jobs to work soley on the increasing need for CXIT's 
-			services. </p>
-			<p>It didn't take long for the two to realise they needed more hands 
-			to help out, and hired Alan, to assist in customer service and larger 
-			scale jobs. The company moved once more, to a larger building in Nelson, 
-			to accomodate the increasing number of server towers. CXIT now consists 
-			of 4 knowlegable, skilled and able-bodied staff and have recently introduced 
-			a website hosting service, to increase their customer base further.
-			</p>
-			<p>Here at CXIT, we offer the friendliest, fastest and most reliable 
-			services available. We look forward to working with any and all of you 
-			to help out with your needs</p>
+			<p></p>
 		</div>
 
 	<div class="row">
@@ -37,57 +15,31 @@
 		<div class="col-sm-12 col-md-6 col-lg-6">
 			<img alt="" class="icon" src="../../public/images/1.png">
 			<h3>John Steviki</h3>
-			<p>From a young age, John has been fascinated by the world of computers 
-			and technology, so it was only a matter of time before he wanted to 
-			make a career out of his hobby.<br>John has been working in the IT industry 
-			for over 17 years and has skills in areas such as tech support, network 
-			engineering and web development</p>
+			<p>Adipisicing irure do nisi consequat nostrud ullamco occaecat amet minim laboris exercitation tempor. 
+				Ex qui in est esse adipisicing consequat nisi minim laborum ipsum sint. 
+				Commodo culpa nisi magna labore anim irure aliquip ex laboris occaecat incididunt consectetur. 
+				Sit anim eu esse irure. Culpa culpa enim cupidatat consequat.<br></p>
 		</div>
 		<div class="col-sm-12 col-md-6 col-lg-6">
 			<img alt="" class="icon" src="../../public/images/2.png">
 			<h3>Blake Stevens</h3>
-			<p>Blake has an undying love for technology that has pushed him further 
-			and further into the IT industry.</p>
-			<p>He spent most of his career prior to CXIT as an administrator, network 
-			engineer and in tech support since 1990, and has worked alongside John 
-			for Johns' entire IT carrer</p>
+			<p></p>
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-md-12 col-lg-6 ">
 			<img alt="" class="icon" src="../../public/images/3.png">
-			<h3>Alan Daniels</h3>
-			<p>Alan moved to New Zealand from Sweden to pursue his IT career. He 
-			gratuated his from his studies in 2005 and has been working in tech 
-			support and networking ever since.</p>
-			<p>He joined the CXIT team in 2013 and has never been happier with his 
-			decision to create a new life in good ol' New Zealan</p>
+			<h3>Alana Daniels</h3>
+			<p></p>
 		</div>
 		<div class="col-md-12 col-lg-6">
 			<img alt="" class="icon" src="../../public/images/4.png">
-			<h3>Danny Stevens</h3>
-			<p>Danny is Blakes son, so a career in IT was inevitable. Danny gratuated 
-			from his IT degree at NMIT in 2017 and is quickly becoming well known 
-			in Nelson's IT industry</p>
-			<p>Danny majored in software development, but is currently CXIT's main 
-			web maintenance tech and administrator. He has plans to incorperate 
-			his software development skills' into CXIT, but we can't yet give any 
-			information on this topic</p>
+			<h3>Danielle Beck</h3>
+			<p></p>
 		</div>
 	</div>
-
-
-
-
-
-
 	</div>
 	<!--About End-->
-	
-
-
-
-
 </template>
 
 
