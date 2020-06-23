7d618854a3a805e2024df2eb2d84c9080fb8a598
jkdearnley <jkdearnley@gmail.com>
2020-06-23 12:28:51 +1200

    Background image and cards

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -2,14 +2,7 @@ import firebase from 'firebase'
 
 var firebaseConfig = {
 // firebase apiKeys
-apiKey: "AIzaSyB8jvXH_hihtt1DjdTsY4h3aiRO_QF5N6w",
-authDomain: "web701-eggcellent.firebaseapp.com",
-databaseURL: "https://web701-eggcellent.firebaseio.com",
-projectId: "web701-eggcellent",
-storageBucket: "web701-eggcellent.appspot.com",
-messagingSenderId: "101664882857",
-appId: "1:101664882857:web:3c9c0ed83efe0dbf2ea9b8"
-};
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
